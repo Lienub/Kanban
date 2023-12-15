d3e25dbe7fc81d420d7ae6d901bfe1b03c69b3a7
mwawrzyniak <mwawrzyniak@unistra.fr>
2023-12-15 18:54:09 +0100

    styles(homepage): implement background design with cork board + add row kanban div in parent div to fix drag and drop

--- src/main.js
+++ src/main.js
@@ -3,7 +3,7 @@ import {
   closeFormToCreateTask,
   saveTask,
   dropTask,
-  allowDropTask
+  allowDropTask,
 } from "./modules/tasks/tasks-event-handlers.js";
 import { eventSaveTag } from "./modules/tags/tags-event-handlers.js";
 import { eventSaveAssignment } from "./modules/assignments/assignments-event-handlers.js";
@@ -12,57 +12,65 @@ document.querySelector("#app").innerHTML = `
 <header>
     <h1>Kanban Oueb</h1>
 </header>
-<main>
-    <div>
-        <button id="create-task-button">Créer une tâche</button>
-    </div>
-    <section id="todo">
+<main class="container">
+<section id="backlog" class="kanban-container">
+    <div class="row-kanban-block">
         <h2>TODO</h2>
-    </section>
-    <section id="wip">
-        <h2>WIP</h2>
-    </section>
-    <section id="done">
-        <h2>DONE</h2>
-    </section>
-    <section>
-        <div id="create-new-task-form">
-            <button id="close-button">X</button>
-            <h2>Nouvelle tache</h2>
-            <div>
-                <label for="task-name">Titre</label>
-                <input type="text" name="task-name" id="task-name">
-            </div>
-            <div>
-                <label for="task-description">Description</label>
-                <textarea name="task-description" id="task-description" cols="70" rows="10"></textarea>
-            </div>
-            <div>
-                <label for="task-start-date">Date de début</label>
-                <input type="date" name="task-start-date" id="task-start-date">
-            </div>
-            <div>
-                <label for="task-end-date">Date de fin</label>
-                <input type="date" name="task-end-date" id="task-end-date">
-            </div>
-            <div>
-                <label for="task-assignment">Affectation(s)</label>
-                <input type="text" name="task-assignment" id="task-assignment">
-                <div id="assignments-block"></div>
-            </div>
-            <div>
-                <label for="task-tag">Tag</label>
-                <input type="text" name="task-tag" id="task-tag">
-                <div id="tags-block"></div>
-            </div>
-            <div>
-                <label for="task-name">Code couleur</label>
-                <input type="color" name="task-code-color" id="task-code-color">
-            </div>
-            <div>
-                <button id="save-button">Save</button>
-            </div>
+        <div id="todo" class="row-kanban">
         </div>
+    </div>
+    <div class="row-kanban-block">
+        <h2>WIP</h2>
+        <div id="wip" class="row-kanban">
+        </div>
+    </div>
+    <div class="row-kanban-block">
+        <h2>DONE</h2>
+        <div id="done" class="row-kanban">
+        </div>
+    </div>
+    <div class="create-task-block">
+       <button id="create-task-button" class="create-new-task-button"><span><i class="fa-solid fa-plus"></i> Créer une tâche</span></button>
+    </div>
+</section>
+<section>
+    <div id="create-new-task-form">
+        <button id="close-button">X</button>
+        <h2>Nouvelle tache</h2>
+        <div>
+            <label for="task-name">Titre</label>
+            <input type="text" name="task-name" id="task-name">
+        </div>
+        <div>
+            <label for="task-description">Description</label>
+            <textarea name="task-description" id="task-description" cols="70" rows="10"></textarea>
+        </div>
+        <div>
+            <label for="task-start-date">Date de début</label>
+            <input type="date" name="task-start-date" id="task-start-date">
+        </div>
+        <div>
+            <label for="task-end-date">Date de fin</label>
+            <input type="date" name="task-end-date" id="task-end-date">
+        </div>
+        <div>
+            <label for="task-assignment">Affectation(s)</label>
+            <input type="text" name="task-assignment" id="task-assignment">
+            <div id="assignments-block"></div>
+        </div>
+        <div>
+            <label for="task-tag">Tag</label>
+            <input type="text" name="task-tag" id="task-tag">
+            <div id="tags-block"></div>
+        </div>
+        <div>
+            <label for="task-name">Code couleur</label>
+            <input type="color" name="task-code-color" id="task-code-color">
+        </div>
+        <div>
+            <button id="save-button">Save</button>
+        </div>
+    </div>
     </section>
 </main>
 `;
