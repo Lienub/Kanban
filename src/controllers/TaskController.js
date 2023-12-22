28f12d97e4ef1683dce3caa90db3372420610262
mwawrzyniak <mwawrzyniak@unistra.fr>
2023-12-22 13:58:31 +0100

    fix(app): change note to notes

--- src/controllers/TaskController.js
+++ src/controllers/TaskController.js
@@ -118,7 +118,7 @@ export default class TaskController {
     var taskStatus = taskDiv.parentElement.id;
     // Delete old task
     taskDiv.remove();
-    let taskNote = document.getElementById('task-details-note').value
+    let taskNotes = document.getElementById('task-details-note').value
 
     var newTaskModel = new TaskModel(
       taskId,
@@ -134,7 +134,7 @@ export default class TaskController {
         : taskStatus == "wip"
           ? StatusEnum.WIP
           : StatusEnum.DONE,
-      taskNote
+      taskNotes
     );
     this.modifyTask(newTaskModel);
   }
